d7ad37b4ab71d7eca704344ac28e247ef7dabec2
fcaquard <fcaquard@student.s19.be>
2021-07-03 14:12:21 +0200

    v1.2 (added forever blocked list)

--- block-site/background.js
+++ block-site/background.js
@@ -1,11 +1,21 @@
 chrome.runtime.onInstalled.addListener(function () {
-	chrome.storage.local.get(["blocked", "duration"], function (local) {
+	chrome.storage.local.get(["blocked", "deadline", "forever", "locked"], function (local) {
 		if (!Array.isArray(local.blocked)) {
 			chrome.storage.local.set({ blocked: [] });
 		}
 
-		if (!(local.duration instanceof Date && !isNaN(local.duration.valueOf()))) {
-			chrome.storage.local.set({ duration: Date.now() });
+		if (!Array.isArray(local.forever)) {
+			chrome.storage.local.set({ forever: [] });
+		}
+		
+		if (!local.deadline) {
+			chrome.storage.local.set({ deadline: Date.now()});
+		}
+
+		if(typeof local.locked !== "boolean")
+		{
+			console.log("NOT BOOLEAN");
+			chrome.storage.local.set({locked: false});
 		}
 	});
 });
@@ -15,13 +25,18 @@ chrome.tabs.onUpdated.addListener(function (tabId, changeInfo) {
 	if (!url || !url.startsWith("http")) {
 		return;
 	}
-
 	const hostname = new URL(url).hostname;
-
-	chrome.storage.local.get(["blocked"], function (local) {
-		const { blocked} = local;
-		if (Array.isArray(blocked) && blocked.find(domain => hostname.includes(domain))) {
+	chrome.storage.local.get(["blocked", "forever", "deadline", "locked"], function (local) {
+		const {blocked, forever, deadline, locked} = local;
+		const now = Date.now();
+		const limit = new Date(deadline);
+		if (locked && limit - now > 0 && Array.isArray(blocked) && blocked.find(domain => hostname.includes(domain))) {
 			chrome.tabs.remove(tabId);
+			return;
+		}
+		if (Array.isArray(forever) && forever.find(domain => hostname.includes(domain))) {
+			chrome.tabs.remove(tabId);
+			return;
 		}
 	});
 });
--- block-site/options.js
+++ block-site/options.js
@@ -1,68 +1,146 @@
-const textareaadd = document.getElementById("textarea-add");
-const textareaactive = document.getElementById("textarea-active");
-const save = document.getElementById("save");
-const update = document.getElementById("update");
-const lock = document.getElementById("lock");
-const timefield = document.getElementById("timefield");
+const addArea = document.getElementById("add-area");
+const activeArea = document.getElementById("active-area");
+const foreverArea = document.getElementById("forever-area");
+const temporaryBtn = document.getElementById("temporary-btn");
+const foreverBtn = document.getElementById("forever-btn");
+const updateBtn = document.getElementById("update-btn");
+const lockBtn = document.getElementById("lock-btn");
+const displayDeadline = document.getElementById("display-deadline");
+const dateDeadline = document.getElementById("date-deadline-input");
+const timeDeadline = document.getElementById("time-deadline-input");
 
-save.addEventListener("click", () => {
-	let content = textareaadd.value;
-    chrome.storage.local.get(["blocked"], function (local) {
-        textareaadd.value = "";
-        if(Array.isArray(local.blocked))
-        {
-            local.blocked.forEach(el => {
-                content += "\n" + el;
-            });
-        }
-        textareaactive.value = content.trim();
-        const blocked = content.split("\n").map(s => s.trim()).filter(Boolean);
-        chrome.storage.local.set({ blocked });
-    });
-});
-
-update.addEventListener("click", () => {
-    const blocked = textareaactive.value.split("\n").map(s => s.trim()).filter(Boolean);
-    chrome.storage.local.set({ blocked });
-  });
-
-lock.addEventListener("click", (event) => {
-	const datenow = Date.now();
-	let duration = datenow + (90 * 24 * 60 * 60 * 1000);
-	chrome.storage.local.set({ duration });
-	hideElements();
-});
-
+// On options page load
 window.addEventListener("DOMContentLoaded", () => {
-  chrome.storage.local.get(["blocked", "duration"], function (local) {
-    const { blocked, duration } = local;
-    if (Array.isArray(blocked)) {
-      textareaactive.value = blocked.join("\n");
-      const date = new Date(duration);
-      if(duration - Date.now() <= 0)
-      {
-		displayElement();
-      }
-      else
-      {
-        const content = document.createTextNode(`Locked for 90 days until: ${date.toDateString()}.`);
-        timefield.appendChild(content);
-      }
-    }
+  chrome.storage.local.get(["blocked", "forever", "deadline", "locked"], function (local) {
+    const { blocked, forever, deadline, locked} = local;
+	// console.log(local);
+	const format = new Date(deadline);
+	
+	dateDeadline.value = getDate(format);
+	timeDeadline.value = getTime(format);
+	if (Array.isArray(blocked)) {
+		activeArea.value = blocked.join("\n");
+	}
+	if (Array.isArray(forever)) {
+      foreverArea.value = forever.join("\n");
+	}
+	if (!locked || deadline - Date.now() <= 0)
+	{
+		chrome.storage.local.set({ locked: false });
+		hideElements(false);
+	}
+	else
+	{
+		const content = document.createTextNode(`Locked until: ${getDate(format)} ${getTime(format)}.`);
+		displayDeadline.appendChild(content);
+	}
   });
 });
 
-function hideElements()
+//  add domain to temporary block list 
+temporaryBtn.addEventListener("click", () => {
+	let content = addArea.value.trim();
+	if(content != "")
+	{
+		chrome.storage.local.get(["blocked"], function (local) {
+			addArea.value = "";
+			if (Array.isArray(local.blocked))
+			{
+				local.blocked.forEach(el => {
+					content += "\n" + el;
+				});
+			}
+			activeArea.value = content.trim();
+			const blocked = content.split("\n").map(s => s.trim()).filter(Boolean);
+			chrome.storage.local.set({ blocked });
+		});
+	}
+});
+
+// add domain to forever block list
+foreverBtn.addEventListener("click", () => {
+	let content = addArea.value.trim();
+	if(content != "")
+	{
+		if (window.confirm('This action will block the following domains forever: \n\n'+content+'\n\nThere\'s no way back.\nContinue anyway ?'))
+		{
+			chrome.storage.local.get(["forever"], function (local) {
+				addArea.value = "";
+				if (Array.isArray(local.forever))
+				{
+					local.forever.forEach(el => {
+						content += "\n" + el;
+					});
+				}
+				foreverArea.value = content.trim();
+				const forever = content.split("\n").map(s => s.trim()).filter(Boolean);
+				chrome.storage.local.set({ forever });
+			});
+		}
+	}
+});
+
+// on change to deadline's day
+dateDeadline.addEventListener("change",()=>{
+	chrome.storage.local.set({ deadline: createDeadline()});
+});
+
+// on change to deadline's time
+timeDeadline.addEventListener("change",()=>{
+	chrome.storage.local.set({ deadline: createDeadline() });
+});
+
+// update temporary blocking list
+updateBtn.addEventListener("click", () => {
+  const blocked = activeArea.value.split("\n").map(s => s.trim()).filter(Boolean);
+  chrome.storage.local.set({ blocked });
+});
+
+// lock the temporary list until the selected deadline date&time
+lockBtn.addEventListener("click", () => {
+	chrome.storage.local.get(["deadline"], function (local) {
+		const now = Date.now();
+		if (local.deadline - now > 0)
+		{
+			chrome.storage.local.set({ locked: true });
+			hideElements(true);
+		}
+	});
+});
+
+// returns the proper date+time timestamp
+function createDeadline()
 {
-	const hidden = document.querySelectorAll(".hidden");
-	hidden.forEach(el => {
-		el.style.display = "none";
-	})	
+	const str = dateDeadline.value + ", "+timeDeadline.value;
+	let deadline = new Date(str);
+	return Date.parse(deadline);
 }
-function displayElement()
+
+// returns HH:mm from deadline
+function getTime(format)
 {
+	const timezone = format.getTimezoneOffset();
+	const hours = parseInt(format.getUTCHours()) - (parseInt(timezone) / 60);
+	let minutes = format.getUTCMinutes();
+	if (minutes < 10)
+	{
+		minutes = "0" + minutes;
+	}
+	return (hours + ":" + minutes);
+}
+
+// returns YYYY-mm-dd from deadline
+function getDate(format)
+{
+	return format.toISOString().split('T')[0];
+}
+
+// toggle .hidden DOM elements
+function hideElements(toggle)
+{
+	const display = (toggle) ? "none" : "block";
 	const hidden = document.querySelectorAll(".hidden");
 	hidden.forEach(el => {
-		el.style.display = "block";
-	})
+		el.style.display = display;
+	})	
 }
